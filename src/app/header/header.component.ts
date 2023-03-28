d33584f99991157cd737745b8d4f5eeb56881635
DawidSh <92631685+DawidSh@users.noreply.github.com>
2023-03-28 13:11:04 +0200

    fix routing for links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({    
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-    @Output() shoppingClicked = new EventEmitter<boolean>();
-    
-    recipesClick()  {
-        this.shoppingClicked.emit(false);  
-    }
-    shoppingClick() {
-        this.shoppingClicked.emit(true);
-    }
-}
+export class HeaderComponent {}
